b52f0923af0d94b2ac9dd31e652d07dd6fd07123
sudip <paudelsudip134@gmail.com>
2024-07-12 14:55:20 +0545

    backend updates

--- Backend/app.js
+++ Backend/app.js
@@ -20,9 +20,9 @@ const app = express();
 
 app.use(cors({
     // origin : "http://localhost:5173",
-    origin : "https://digital-kirana-liard.vercel.app/",
-    methods:"GET,POST,PUT,DELETE",
-    credentials:true,
+    origin: "https://digital-kirana-liard.vercel.app/",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
 }))
 
 // Configure express-session
@@ -30,9 +30,9 @@ app.use(session({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: false,
-    store:MongoStore.create({
-        mongoUrl:process.env.MONGO_URI,
-        ttl:14 * 24 * 60 *60 // = 14 days expiration
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        ttl: 14 * 24 * 60 * 60 // = 14 days expiration
     })
 }));
 
@@ -41,59 +41,66 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const fileStorage = multer.diskStorage({
-    destination:(req,file,cb)=> {
+    destination: (req, file, cb) => {
         if (file.fieldname === 'imageUrl') {
             cb(null, "../Frontend/Assets/Images/categories/trying");
-        } else if(file.fieldname === 'promoImg'){
+        } else if (file.fieldname === 'promoImg') {
             cb(null, "../Frontend/Assets/Images/promotion");
         } else {
             cb(null, "../Frontend/Assets/Images/Products");
         }
     },
-    filename:(req,file,cb) =>{
-        cb(null,file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
     }
 });
-const fileFilter = (req,file,cb) =>{
-    if(file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
-        cb(null,true)
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
+        cb(null, true)
     } else {
-        cb(null,false);
+        cb(null, false);
     }
 }
 const upload = multer(
     {
-        storage:fileStorage,
-        fileFilter:fileFilter
+        storage: fileStorage,
+        fileFilter: fileFilter
     }
 )
 app.use(express.json())
 app.use(cookieParser())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 const uploadFields = [
     { name: 'frontView' },
     { name: 'backView' },
-    {name:'sideView'},
-    {name:'topView'},
-    {name:'imageUrl'},
-    {name:'promoImg'}
+    { name: 'sideView' },
+    { name: 'topView' },
+    { name: 'imageUrl' },
+    { name: 'promoImg' }
     // Add more fields as needed
 ];
-  
+
 app.use(upload.fields(uploadFields));
 
 const authRoutes = require('./routes/authRoute');
 const adminRoutes = require("./routes/adminRoutes")
 const pageRoutes = require("./routes/pageRoutes");
 
-app.use("/auth",authRoutes);
-app.use("/api",pageRoutes);
-app.use("/admin",adminRoutes)
+app.use("/auth", authRoutes);
+app.use("/api", pageRoutes);
+app.use("/admin", adminRoutes)
 
-const port=process.env.PORT || 3000;
-mongoose.connect(process.env.MONGO_URI).then((result) => {
-    console.log("Connected To DataBase")
-    app.listen(port,()=>{
-        console.log(`Server running on http://localhost:${port}`)
-    })
-})
+const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
+    });
+}).catch(err => {
+    console.error("MongoDB connection error:", err);
+});
+
+
+
+
+
