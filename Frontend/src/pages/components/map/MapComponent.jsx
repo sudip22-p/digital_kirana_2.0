f030e1aac9c28d869baa8af2cfe50fe1c9055940
sudip <paudelsudip134@gmail.com>
2024-07-12 21:11:31 +0545

    vercel upd

--- Frontend/src/pages/components/map/MapComponent.jsx
+++ Frontend/src/pages/components/map/MapComponent.jsx
@@ -2,6 +2,15 @@ import { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+// Manually set path to Leaflet's default icons
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+    iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+});
+
 function Map() {
     useEffect(() => {
         const map = L.map('map').setView([28.2096, 83.9856], 13);
@@ -11,36 +20,7 @@ function Map() {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
-        let marker, circle, zoomed;
         let clickMarker;
-
-        function success(pos) {
-            const lat = pos.coords.latitude;
-            const lng = pos.coords.longitude;
-            const accuracy = pos.coords.accuracy;
-
-            if (marker) {
-                map.removeLayer(marker);
-                map.removeLayer(circle);
-            }
-            marker = L.marker([lat, lng]).addTo(map);
-            circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
-
-            if (!zoomed) {
-                zoomed = map.fitBounds(circle.getBounds());
-            }
-            map.setView([lat, lng]);
-        }
-
-        function error(err) {
-            if (err.code === 1) {
-                alert('Please allow geolocation access');
-            } else {
-                alert('Cannot get current location');
-            }
-        }
-
-        // navigator.geolocation.watchPosition(success, error);
 
         map.on('click', function (e) {
             var lat = e.latlng.lat;
@@ -61,8 +41,6 @@ function Map() {
 
     return (
         <div>
-            {/* <h1>Map user location</h1> */}
-            {/* <h1>Works</h1> */}
             <div id="map" style={{ height: '350px', width: '400px', zIndex: 0 }}></div>
             <p id="info"></p>
         </div>
