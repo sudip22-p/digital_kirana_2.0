e72f7f636f86bedb2f07bf5c424a3f6dadb88166
sudip <paudelsudip134@gmail.com>
2024-07-12 19:07:31 +0545

    vercel upd

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(cors({
     // origin : "http://localhost:5173",
-    origin: 'https://digital-kirana-gules.vercel.app/',
+    origin: 'https://digital-kirana-gules.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
