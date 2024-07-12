8b26d9d9e988c1261e14d6379211f9e584e39238
sudip <paudelsudip134@gmail.com>
2024-07-12 18:53:23 +0545

    vercel upd

--- Backend/app.js
+++ Backend/app.js
@@ -20,8 +20,8 @@ const app = express();
 
 app.use(cors({
     // origin : "http://localhost:5173",
-    origin: "https://digital-kirana-liard.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: 'https://digital-kirana-liard.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
 // Configure express-session
